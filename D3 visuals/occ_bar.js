6e2d687c2a74a1b5ab1aa970285761203e4fe807
Steffen Holter <steffenholter@gmail.com>
2018-07-24 14:18:47 -0400

    Added occurance bar

--- D3 visuals/occ_bar.js
+++ D3 visuals/occ_bar.js
@@ -1,10 +1,16 @@
-function draw_percent_bar(good,bad) {
+function draw_occurance_bar(anch_good,anch_bad,change_good,change_bad) {
     
-    var bar_width = 100,
-        bar_height = 20;
+    var bar_width = 30,
+        bar_height = 5;
     
-    var x_buffer = 10,
-        y_buffer = 10;
+    var shift = bar_height+5
+    
+    var text_space = 30
+    
+    var text_col = "#808080"
+    
+    var x_buffer = 2+text_space,
+        y_buffer = 2;
     
     var xScale = d3.scaleLinear()
             .domain([0, 1])
@@ -14,28 +20,48 @@ function draw_percent_bar(good,bad) {
         bad_col = "#d95f02";
 
     var svg = d3.select("body").append("svg")
-                .attr("width",270)
-                .attr("height",55)
-                .append("g")
-                     .attr("transform","translate(" + x_buffer + ',' + y_buffer +')');
+                .attr("width",175)
+                .attr("height",30)
+    
+   // Change Label
+    svg.append('g').append("text")
+        .text("# anchors")
+        .attr("x",4)
+        .attr("y",bar_height/2+5)
+        .attr("font-family", 'sans-serif')
+        .attr("font-size", '5px')
+        .attr("fill","#808080")
+        .attr("text-anchor",'start');
+    
+    svg.append('g').append("text")
+        .text("# changes")
+        .attr("x",4)
+        .attr("y",bar_height/2+shift+5)
+        .attr("font-family", 'sans-serif')
+        .attr("font-size", '5px')
+        .attr("fill","#808080")
+        .attr("text-anchor",'start');
+    
+    
+    svg = svg.append("g")
+             .attr("transform","translate(" + x_buffer + ',' + y_buffer +')');
     
     // Drawing the frame
     
-    var sep = 2.5
-    var border = 3
+    var sep = 1
+    var border = 1.2
     
-     svg.append('g').append("rect")
+    svg.append('g').append("rect")
         .attr("height",bar_height+sep*2)
         .attr("width",bar_width+border)
         .style("fill","None")
         .style("stroke-width",border)
         .style("stroke","black");
     
-    var svg = svg.append("g")
+    var svg_up = svg.append("g")
                      .attr("transform","translate(" + border + ',' + sep +')');
     
-//    svg.append('g').append("text")
-//        .attr("class","per_marker")
+    svg.append('g').append("text")
 //        .text(function(){return Math.round(result*100).toString() + "%"})
 //        .attr("x",function(){return xScale(result)+5;})
 //        .attr("y",bar_height+20)
@@ -47,19 +73,50 @@ function draw_percent_bar(good,bad) {
 //            else {return bad_col}})
 //        .attr("text-anchor",'middle');
     
-    // Drawing the good recangle
-    svg.append('g').append("rect")
+    // Drawing the anch good recangle
+    svg_up.append('g').append("rect")
         .attr("height",bar_height)
-        .attr("width",xScale(good))
+        .attr("width",xScale(anch_good))
         .style("fill",good_col);
     
-    // Drawing the bad rectangle
-     svg.append('g').append("rect")
-        .attr('x',xScale(good))
+    // Drawing the anch bad rectangle
+     svg_up.append('g').append("rect")
+        .attr('x',xScale(anch_good))
         .attr("height",bar_height)
-        .attr("width",xScale(bad))
+        .attr("width",xScale(anch_bad))
         .style("fill",bad_col);
-
+    
+    
+    
+    // Second part
+    
+    svg.append('g').append("rect")
+        .attr('y',shift)
+        .attr("height",bar_height+sep*2)
+        .attr("width",bar_width+border)
+        .style("fill","None")
+        .style("stroke-width",border)
+        .style("stroke","black");
+    
+    var svg_down = svg.append("g")
+                     .attr("transform","translate(" + border + ',' + sep +')');
+    
+    // Drawing the change good recangle
+    svg_down.append('g').append("rect")
+        .attr('y',shift)
+        .attr("height",bar_height)
+        .attr("width",xScale(change_good))
+        .style("fill",good_col);
+    
+    // Drawing the change bad rectangle
+     svg_down.append('g').append("rect")
+        .attr('y',shift)
+        .attr('x',xScale(change_good))
+        .attr("height",bar_height)
+        .attr("width",xScale(change_bad))
+        .style("fill",bad_col);
+    
+    
 }
 
-draw_percent_bar(0.29,0.15);
+draw_occurance_bar(0.29,0.15,0.3,0.5);
